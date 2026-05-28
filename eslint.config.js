3aad8b4779240d50aeb3c63d9b122e058e28ae5e
Achmad Aidhil <achmadaidhil2@gmail.com>
2026-05-28 22:36:11 +0800

    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import js from "@eslint/js";
+import globals from "globals";
+// import { defineConfig } from "eslint/config";
+import daStyle from "eslint-config-dicodingacademy";
+
+export default defineConfig([
+  daStyle,
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: { sourceType: "module" },
+  },
+  // extends: ["js/recommended"],
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+]);
