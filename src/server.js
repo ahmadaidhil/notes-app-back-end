d2b7894a2ed19074009a1ee5d126c45a8b6a702a
Achmad Aidhil <achmadaidhil2@gmail.com>
2026-06-01 19:16:35 +0800

    refactor: restructure notes service and API routes

--- src/server.js
+++ src/server.js
@@ -1,17 +1,8 @@
-import express from "express";
-import routes from "./routes.js";
-import cors from "cors";
-
-const app = express();
-const port = process.env.PORT || 3000;
-const host = process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0";
-
-app.use(express.json());
-app.use(cors({
-  origin: '*'
-}));
-app.use('/', routes);
+import server from './server/index.js';
  
-app.listen(port, () => {
-  console.log(`Server running at http://${host}:${port}`);
+const host = process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0';
+const port = process.env.PORT || 3000;
+ 
+server.listen(port, () => {
+ console.log(`Server running at http://${host}:${port}`);
 });
--- src/services/notes/routes/index.js
+++ src/services/notes/routes/index.js
@@ -5,7 +5,7 @@ import {
   editNoteById,
   getNotes,
   getNoteById,
-} from './controller.js';
+} from '../controller/controller.js';
 
 const router = express.Router();
 
