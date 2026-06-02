61827716a1df471575cf9763e477337fe1584395
Achmad Aidhil <achmadaidhil2@gmail.com>
2026-06-02 18:32:25 +0800

    feat: add Joi validation for note payload and enhance error handling

--- src/services/notes/routes/index.js
+++ src/services/notes/routes/index.js
@@ -1,18 +1,16 @@
-import express from "express";
+import express from 'express';
 import {
-  createNote,
-  deleteNoteById,
-  editNoteById,
-  getNotes,
-  getNoteById,
-} from '../controller/controller.js';
-
+createNote, getAllNotes, getNoteById, editNoteById, deleteNoteById
+} from '../controller/note-controller.js';
+import validate from '../../../middlewares/validate.js';
+import { notePayloadSchema } from '../../../services/notes/validator/schema.js';
+ 
 const router = express.Router();
-
-router.post("/notes", createNote);
-router.get('/notes', getNotes);
+ 
+router.post('/notes', validate(notePayloadSchema), createNote);
+router.get('/notes', getAllNotes);
 router.get('/notes/:id', getNoteById);
-router.put('/notes/:id', editNoteById);
+router.put('/notes/:id', validate(notePayloadSchema), editNoteById);
 router.delete('/notes/:id', deleteNoteById);
-
+ 
 export default router;
