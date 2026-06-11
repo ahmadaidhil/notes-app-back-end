99fd0674951c7fce3e902a24f70cfd140e7cb5f5
Achmad Aidhil <achmadaidhil2@gmail.com>
2026-06-11 20:49:30 +0800

    feat: implement note repository and migration for notes table, refactor controller logic

--- src/services/notes/routes/index.js
+++ src/services/notes/routes/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createNote, getNotes, getNoteById, editNoteById, deleteNoteById } from '../controller/controller.js';
+import { createNote, getNotes, getNoteById, editNoteById, deleteNoteById } from '../controller/note-controlle.js';
 import validate from '../../../middlewares/validate.js';
 import { notePayloadSchema } from '../../../services/notes/validator/schema.js';
  
